ee854aa3f1bee3b5ecbd6d24b0bcd26fd6f3ff53
August Dolan <august.dolan1@gmail.com>
2020-10-21 19:56:24 -0700

    Fix postgres db directory, create cassandra one

--- db/cassandra/index.js
+++ db/cassandra/index.js
@@ -1,30 +1,21 @@
 const cassandra = require('cassandra-driver');
+const {performance} = require('perf_hooks');
 
 const client = new cassandra.Client({
   contactPoints: ['127.0.0.1'],
   localDataCenter: 'datacenter1',
+  keyspace: 'sdc',
 })
 
-client.connect()
+let t1 = performance.now();
+client.execute('SELECT * FROM search_by_product WHERE id=9999999')
   .then((results) => {
-    console.log('Connected to cassandra container');
-    client.execute("DROP KEYSPACE IF EXISTS sdc")
-    .then(() => {
-      client.execute("CREATE KEYSPACE sdc WITH REPLICATION={'class': 'SimpleStrategy', 'replication_factor': 1};")
-      .then(() => {
-        client.execute('USE sdc')
-          .then(() => {
-            client.execute('CREATE TABLE products (test varchar, test2 varchar, PRIMARY KEY (test))')
-              .then(() => {
-                client.execute("INSERT INTO products (test, test2) VALUES ('hotdog', 'macaroni')")
-                  .then(() => {
-                    client.execute('SELECT * FROM products')
-                      .then((results) => {
-                        console.log(results);
-                      })
-                  })
-              })
-          })
+    const { brand_id, dept_id } = results.rows[0];
+    client.execute(`SELECT * FROM products WHERE dept_id=${dept_id} AND brand_id>=${brand_id} LIMIT 35`)
+      .then((results) => {
+        console.log(results);
+        let t2 = performance.now();
+        console.log(t2 - t1);
       })
       .catch((err) => {
         console.log(err);
@@ -32,7 +23,20 @@ client.connect()
 
   })
   .catch((err) => {
-    console.log('error connecting to the cassandra container');
     console.log(err);
   })
+
+module.exports = {
+  // basic query structure for promises
+  execute: (text, params) => {
+    return new Promise((resolve, reject) => {
+      client.execute(text, params)
+        .then((results) => {
+          resolve(results);
+        })
+        .catch((err) => {
+          reject(err);
+        })
     })
+  }
+}
